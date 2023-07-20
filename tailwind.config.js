d4fac79ee2e396ffd713a2ea65856cb61c0e2b21
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-07-21 02:50:17 +0900

    chore: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        header: '80px',
+      },
+      width: {
+        lg: '960px',
+      },
+    },
   },
   plugins: [],
 };
