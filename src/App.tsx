174179f0f0fcb8db17ebba1eb2279d5c179eb038
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-07-21 01:58:58 +0900

    feat: Set queryProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
 export default App;
