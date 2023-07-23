f8971294f68b59830fe2bc07afc6d339afb1b548
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2023-07-24 00:36:53 +0900

    chore: Proxy setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/period': {
+        target: 'http://www.culture.go.kr',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/period/, ''),
+      },
+    },
+  },
 });
